03b9100d57daf0980335fe69eab5a7f6f0e4fe6f
kenneth-okoro <kennethokesiena@gmail.com>
2022-12-24 06:13:23 +0100

    Installed and setup stripe

--- components/Cart.js
+++ components/Cart.js
@@ -6,14 +6,45 @@ import {
   CartWrapper,
   EmptyStyle,
   Checkout,
+  Cards,
 } from "../styles/CartStyle";
 import { Quantity } from "../styles/ProductDetails";
 import { FaShoppingCart } from "react-icons/fa";
 import { AiFillMinusCircle, AiFillPlusCircle } from "react-icons/ai";
+import getStripe from "../lib/getStripe";
+
+// Animation Variants
+const card = {
+  hidden: { opacity: 0, scale: 0.8 },
+  show: { opacity: 1, scale: 1 },
+};
+
+const cards = {
+  hidden: { opacity: 1 },
+  show: {
+    opacity: 1,
+    transition: {
+      delayChildren: 0.4,
+      staggerChildren: 0.1,
+    },
+  },
+};
 
 export default function Cart() {
   const { cartItems, setShowCart, onAdd, onRemove, totalPrice } =
     useStateContext();
+
+  // Payment
+  const handleCheckout = async () => {
+    const stripe = await getStripe();
+    const response = await fetch("/api/stripe", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(cartItems),
+    });
+    const data = await response.json();
+    await stripe.redirectToCheckout({ sessionId: data.id });
+  };
 
   return (
     <CartWrapper
@@ -39,40 +70,37 @@ export default function Cart() {
             <FaShoppingCart />
           </EmptyStyle>
         )}
-        {cartItems.length >= 1 &&
-          cartItems.map((item) => {
-            return (
-              <Card
-                initial={{ opacity: 0, scale: 0.8 }}
-                animate={{ opacity: 1, scale: 1 }}
-                transition={{ delay: 0.3 }}
-                key={item.slug}
-              >
-                <img
-                  src={item.image.data.attributes.formats.small.url}
-                  alt={item.title}
-                />
-                <CardInfo>
-                  <h3>{item.title}</h3>
-                  <h3>${item.price}</h3>
-                  <Quantity>
-                    <span>Quantity</span>
-                    <button onClick={() => onRemove(item)}>
-                      <AiFillMinusCircle />
-                    </button>
-                    <p>{item.quantity}</p>
-                    <button onClick={() => onAdd(item, 1)}>
-                      <AiFillPlusCircle />
-                    </button>
-                  </Quantity>
-                </CardInfo>
-              </Card>
-            );
-          })}
+        <Cards layout variants={cards} initial="hidden" animate="show">
+          {cartItems.length >= 1 &&
+            cartItems.map((item) => {
+              return (
+                <Card layout variants={card} key={item.slug}>
+                  <img
+                    src={item.image.data.attributes.formats.small.url}
+                    alt={item.title}
+                  />
+                  <CardInfo>
+                    <h3>{item.title}</h3>
+                    <h3>${item.price}</h3>
+                    <Quantity>
+                      <span>Quantity</span>
+                      <button onClick={() => onRemove(item)}>
+                        <AiFillMinusCircle />
+                      </button>
+                      <p>{item.quantity}</p>
+                      <button onClick={() => onAdd(item, 1)}>
+                        <AiFillPlusCircle />
+                      </button>
+                    </Quantity>
+                  </CardInfo>
+                </Card>
+              );
+            })}
+        </Cards>
         {cartItems.length >= 1 && (
-          <Checkout>
+          <Checkout layout>
             <h3>Subtotal: ${totalPrice} </h3>
-            <button>Purchase</button>
+            <button onClick={handleCheckout}>Purchase</button>
           </Checkout>
         )}
       </CartStyle>
---
+++ pages/api/stripe.js
@@ -0,0 +1,44 @@
+import Stripe from "stripe";
+
+const stripe = new Stripe(`${process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY}`);
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    try {
+      // Create Checkout Session
+      const session = await stripe.checkout.sessions.create({
+        submit_type: "pay",
+        mode: "payment",
+        payment_method_types: ["card"],
+        shipping_address_collection: {
+          allowed_countries: ["US", "CA", "GB", "NG"],
+        },
+        line_items: req.body.map((item) => {
+          return {
+            price_data: {
+              currency: "usd",
+              product_data: {
+                name: item.title,
+                images: [item.image.data.attributes.formats.thumbnail.url],
+              },
+              unit_amount: item.price * 100,
+            },
+            adjustable_quantity: {
+              enabled: true,
+              minimum: 1,
+            },
+            quantity: item.quantity,
+          };
+        }),
+        // Success Order Page
+        success_url: `${req.headers.origin}/success?&session_id={CHECKOUT_SESSION_ID}`,
+
+        // Cancel Order Page
+        cancel_url: `${req.headers.origin}/cancelled`,
+      });
+      res.status(200).json(session);
+    } catch (error) {
+      res.status(error.statusCode || 500).json(error.message);
+    }
+  }
+}
